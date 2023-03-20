28a08af79dda64f596351672f577929aee67415a
mungai <cmonnnganga@gmail.com>
2023-03-20 11:25:58 +0300

    A few minor improvements

--- static/js/main.js
+++ static/js/main.js
@@ -3,3 +3,25 @@ const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstra
 const popover = new bootstrap.Popover('.popover-dismiss', {
     trigger: 'focus'
 })
+
+/*alert template*/
+const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+
+const alert = (message, type) => {
+    const wrapper = document.createElement('div')
+    wrapper.innerHTML = [
+        `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+        `   <div>${message}</div>`,
+        '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+        '</div>'
+    ].join('')
+
+    alertPlaceholder.append(wrapper)
+}
+
+const alertTrigger = document.getElementById('liveAlertBtn')
+if (alertTrigger) {
+    alertTrigger.addEventListener('click', () => {
+        alert('Nice, you triggered this alert message!', 'success')
+    })
+}
